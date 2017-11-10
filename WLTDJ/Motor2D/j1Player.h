9cb9206fd2f2a17d0306d2210bfee82ac66adb41
retsnom9 <retsnom797@gmail.com>
2017-11-10 17:08:50 +0100

    Implemented super godmode (F10 for godmode, LShift+F10 for super godmode)

--- WLTDJ/Motor2D/j1Player.h
+++ WLTDJ/Motor2D/j1Player.h
@@ -47,6 +47,7 @@ public:
 	bool sliding = false;
 	bool win = false;
 	bool godmode = false;
+	bool super_godmode = false;
 	int jcontact = 0; // for Jump() function
 
 	iPoint contact; // y = 0 -> none, y = 1 -> ground, y = 2 -> ceiling /// x = 0 -> none, x = 1 -> left, x = 2 -> right
